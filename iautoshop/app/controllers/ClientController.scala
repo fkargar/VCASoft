62eb4821d0e7fcad65fa1db0d1fce043113a07fb
fkargar <fkargar@yahoo.com>
2017-12-15 16:19:46 -0500

    Renamed classes for controllers class. All must be ClassnameContorler

--- iautoshop/app/controllers/ClientController.scala
+++ iautoshop/app/controllers/ClientController.scala
@@ -10,7 +10,7 @@ import models.Client
  * application's home page.
  */
 @Singleton
-class Client @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ClientController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page.
@@ -20,12 +20,12 @@ class Client @Inject()(cc: ControllerComponents) extends AbstractController(cc) 
    * a path of `/`.
    */
   
-  def client1() = Action { 
+  def client1() = Action { implicit request: Request[AnyContent] =>
     val cl = Client("Farshad", "Kargar")
     Ok(views.html.client(cl))
   }
   
-  def client(fname: String) = Action { 
+  def client(fname: String) = Action { implicit request: Request[AnyContent] =>
     val cl = Client("test", fname)
     Ok(views.html.client(cl))
   }
